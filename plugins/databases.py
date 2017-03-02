b962d9bb08ed90059af9af84fe399f2e000b7b75
Daniel Aguado <danielaguadoaraujo@gmail.com>
2017-03-02 19:56:45 +0100

    Now databases only try to get config files when try to use it

--- plugins/databases.py
+++ plugins/databases.py
@@ -5,14 +5,22 @@ from .utils import liquibase
 
 class databases(application_deployer):
 
-    def __init__(
-            self, name, app_config, app_type,
-            weight, global_config, config_path, install_path):
-        super(databases, self).__init__(
-            name, app_config, app_type, weight,
-            global_config, config_path, install_path)
+    def _search_properties_file(self):
+        generic_config_file = self._find_files(
+            self.generic_config_path, 'liquibase.properties')
+        specific_config_file = self._find_files(
+            self.specific_config_path, 'liquibase.properties')
 
-        self.properties_file = ''
+        if len(specific_config_file) > 0:
+            return specific_config_file[0]
+        else:
+            if len(generic_config_file) > 0:
+                return generic_config_file[0]
+            else:
+                raise FileNotFoundError(
+                    '{path}/liquibase.properties not found!'.format(
+                        path=self.generic_config_path
+                    ))
 
     def get_installed_version(self):
         super(databases, self).get_installed_version()
@@ -24,27 +32,18 @@ class databases(application_deployer):
                            source=self.global_config['choco_source'],
                            download_only=True)
 
-    def configure(self):
-        super(databases, self).configure()
-
-        generic_config_file = self._find_files(
-            self.generic_config_path, 'liquibase.properties')
-        specific_config_file = self._find_files(
-            self.specific_config_path, 'liquibase.properties')
-
-        if len(specific_config_file) > 0:
-            self.properties_file = specific_config_file[0]
-        else:
-            if len(generic_config_file) > 0:
-                self.properties_file = generic_config_file[0]
-            else:
-                raise FileNotFoundError(
-                    '{path}/liquibase.properties not found!'.format(
-                        path=self.generic_config_path
-                    ))
+    def _execute_update(self, changelog_file_path, properties_file):
+        liquibase.update_database(
+            self.params['host'],
+            self.params['database_name'],
+            changelog_file_path,
+            properties_file,
+            self.params['arguments'] if 'arguments' in self.params else None,
+            self.params['contexts'] if 'contexts' in self.params else None)
 
     def deploy_pre(self):
         super(databases, self).deploy_pre()
+        properties_file = self._search_properties_file()
 
         if 'create_database' in self.params and \
                 self.params['create_database']:
@@ -52,23 +51,13 @@ class databases(application_deployer):
                 self.params['host'],
                 self.params['db_name'],
                 self.install_path + '/changelog-database.xml',
-                self.properties_file)
-
-        liquibase.update_database(
-            self.params['host'],
-            self.params['db_name'],
-            self.install_path + '/changelog-latest-pre.xml',
-            self.properties_file,
-            self.params['arguments'],
-            self.params['contexts'])
+                properties_file)
+        self._execute_update(self.install_path + '/changelog-latest-pre.xml',
+                             properties_file)
 
     def deploy_post(self):
         super(databases, self).deploy_post()
+        properties_file = self._search_properties_file()
 
-        liquibase.update_database(
-            self.params['host'],
-            self.params['db_name'],
-            self.install_path + '/changelog-latest-post.xml',
-            self.properties_file,
-            self.params['arguments'],
-            self.params['contexts'])
+        self._execute_update(self.install_path + '/changelog-latest-post.xml',
+                             properties_file)
